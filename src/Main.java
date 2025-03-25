7c58acb5a6d61ffebd348365ad4c0ca43dc441e1
Fahueso <fa.huesopastor@edu.gva.es>
2025-03-25 19:43:24 +0100

    Tercer commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     //Texto
     public static void main(String[] args) {
         System.out.println("¡Hola, IntelliJ y GitHub!");
+        System.out.println("¡Hola, IntelliJ y GitHub!");
+
     }
 }
